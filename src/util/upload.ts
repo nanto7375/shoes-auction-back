80f6e1f8058e211f3bb40229b7beb63d4f15bc86
정승우 <seungwoo@jeongseung-uui-MacBookPro.local>
2022-01-07 19:16:50 +0900

    upload test4

--- src/util/upload.ts
+++ src/util/upload.ts
@@ -1,37 +1,37 @@
-// import multer from "multer";
-// import moment from "moment";
+import multer from "multer";
+import moment from "moment";
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "./public/uploads");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, moment().format("YYYYMMDDHHmmss") + "_" + file.originalname); // 저장되는 파일명
-//   },
-// });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, moment().format("YYYYMMDDHHmmss") + "_" + file.originalname); // 저장되는 파일명
+  },
+});
 
-// export const upload = multer({ storage: storage }).single("file");
+export const upload = multer({ storage: storage }).single("file");
 
 // -----------------------------------------------
 
-import multer from "multer";
-import multerS3 from "multer-s3";
-import aws from "aws-sdk";
+// import multer from "multer";
+// import multerS3 from "multer-s3";
+// import aws from "aws-sdk";
 
-const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_KEYID,
-  secretAccessKey: process.env.AWS_PRIVATEKEY,
-  region: "ap-northeast-2",
-});
+// const s3 = new aws.S3({
+//   accessKeyId: process.env.AWS_KEYID,
+//   secretAccessKey: process.env.AWS_PRIVATEKEY,
+//   region: "ap-northeast-2",
+// });
 
-export const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: "shoespanda",
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    acl: "public-read",
-    key: (req, file, cb) => {
-      cb(null, `picture/shoePic/${Date.now()}_${file.originalname}`);
-    },
-  }),
-}).single("file");
+// export const upload = multer({
+//   storage: multerS3({
+//     s3: s3,
+//     bucket: "shoespanda",
+//     contentType: multerS3.AUTO_CONTENT_TYPE,
+//     acl: "public-read",
+//     key: (req, file, cb) => {
+//       cb(null, `picture/shoePic/${Date.now()}_${file.originalname}`);
+//     },
+//   }),
+// }).single("file");
