18d3ba0d64c1e9878465e8ce5d8d77ad7caa44dc
정승우 <seungwoo@jeongseung-uui-MacBookPro.local>
2022-01-12 11:19:31 +0900

    v3 test

--- src/util/upload.ts
+++ src/util/upload.ts
@@ -1,3 +1,6 @@
+// console.log(process.memoryUsage());
+
+/* db server에 업로드 */
 // import multer from "multer";
 // import moment from "moment";
 
@@ -12,43 +15,43 @@
 
 // export const upload = multer({ storage: storage }).single("file");
 
-// -----------------------------------------------
+/* s3 업로드 aws-sdk v2 */
 
-import multer from "multer";
-import multerS3 from "multer-s3";
+// import multer from "multer";
+// import multerS3 from "multer-s3";
 
-import S3 from "aws-sdk/clients/s3";
+// import S3 from "aws-sdk/clients/s3";
 
-const s3 = new S3({
-  accessKeyId: process.env.AWS_KEYID,
-  secretAccessKey: process.env.AWS_PRIVATEKEY,
-  region: "ap-northeast-2",
-});
+// const s3 = new S3({
+//   accessKeyId: process.env.AWS_KEYID,
+//   secretAccessKey: process.env.AWS_PRIVATEKEY,
+//   region: "ap-northeast-2",
+// });
 
-export const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: "shoespanda",
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    acl: "public-read",
-    key: (req, file, cb) => {
-      cb(null, `picture/shoePic/${Date.now()}_${file.originalname}`);
-    },
-  }),
-}).single("file");
+// export const upload = multer({
+//   storage: multerS3({
+//     s3: s3,
+//     bucket: "shoespanda",
+//     contentType: multerS3.AUTO_CONTENT_TYPE,
+//     acl: "public-read",
+//     key: (req, file, cb) => {
+//       cb(null, `picture/shoePic/${Date.now()}_${file.originalname}`);
+//     },
+//   }),
+// }).single("file");
+
+/* s3 업로드 aws-sdk v3 */
+
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+export const s3Client = new S3Client({ region: "ap-northeast-2" });
+
+export const upload = (req, res, file) => {
+  return {
+    Bucket: "shoespanda",
+    Key: `picture/shoePic/${Date.now()}_${file.originalname as any}`,
+    Body: file,
+  };
+};
 
 // console.log(process.memoryUsage());
-
-// --------------------------------------------------
-
-// import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-
-// export const s3Client = new S3Client({ region: "ap-northeast-2" });
-
-// export const upload = (req, res, file) => {
-//   return {
-//     Bucket: "shoespanda",
-//     Key: `picture/shoePic/${Date.now()}_${file.originalname as any}`,
-//     Body: file,
-//   };
-// };
