43641e5685d69350f10593b0b077cba7d76f3e2f
정승우 <seungwoo@jeongseung-uui-MacBookPro.local>
2022-01-12 15:03:40 +0900

    v3 test pause

--- src/util/upload.ts
+++ src/util/upload.ts
@@ -16,46 +16,44 @@
 // export const upload = multer({ storage: storage }).single("file");
 
 /* s3 업로드 aws-sdk v2 */
-
-// import multer from "multer";
-// import multerS3 from "multer-s3";
-
-// import S3 from "aws-sdk/clients/s3";
-
-// const s3 = new S3({
-//   accessKeyId: process.env.AWS_KEYID,
-//   secretAccessKey: process.env.AWS_PRIVATEKEY,
-//   region: "ap-northeast-2",
-// });
-
-// export const upload = multer({
-//   storage: multerS3({
-//     s3: s3,
-//     bucket: "shoespanda",
-//     contentType: multerS3.AUTO_CONTENT_TYPE,
-//     acl: "public-read",
-//     key: (req, file, cb) => {
-//       cb(null, `picture/shoePic/${Date.now()}_${file.originalname}`);
-//     },
-//   }),
-// }).single("file");
-
-/* s3 업로드 aws-sdk v3 */
-
 import multer from "multer";
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import multerS3 from "multer-s3";
 
-export const s3Client = new S3Client({ region: "ap-northeast-2" });
+import S3 from "aws-sdk/clients/s3";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, `${Date.now()}_${file.originalname}`);
-  },
+const s3 = new S3({
+  accessKeyId: process.env.AWS_KEYID,
+  secretAccessKey: process.env.AWS_PRIVATEKEY,
+  region: "ap-northeast-2",
 });
 
-export const upload = multer({ storage });
+export const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "shoespanda",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, file, cb) => {
+      cb(null, `picture/shoePic/${Date.now()}_${file.originalname}`);
+    },
+  }),
+}).single("file");
+
+/* s3 업로드 aws-sdk v3 */
+// import multer from "multer";
+// import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+// export const s3Client = new S3Client({ region: "ap-northeast-2" });
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/uploads");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, `${Date.now()}_${file.originalname}`);
+//   },
+// });
+
+// export const upload = multer({ storage });
 
 // console.log(process.memoryUsage());
