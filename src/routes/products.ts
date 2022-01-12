d32f1c1c38b1fc5bcfc393cca7208fb4a5cea923
정승우 <seungwoo@jeongseung-uui-MacBookPro.local>
2022-01-12 14:12:41 +0900

    v3 test15

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -196,6 +196,7 @@ router.post("/upload", (req: Request, res: Response) => {
       console.log("경계");
       const file: any = req.file as Express.Multer.File;
       console.log(file);
+      console.log(file.buffer);
       const filename: string = file.filename;
       // s3에 업로드 제대로 된다. front에서 파일만 가져 와서 적용하면 된다.
       const s3Client = new S3Client({ region: "ap-northeast-2" });
